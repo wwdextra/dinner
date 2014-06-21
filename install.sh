9280d349deb9cf6a0c412493c4e20262a78b450f
Micheal <fanlinsheng@gmail.com>
2014-06-21 23:13:06 +0800

    temp ci

--- install.sh
+++ install.sh
@@ -1,9 +1,21 @@
-sudo apt-get install npm
-sudo npm install yo bower grunt-cli
+sudo apt-get install npm nodejs
+sudo npm install yo bower grunt-cli grunt
 sudo npm install -g generator-webapp
 
-# Init sqlite database
-python ./src/db.py
+sudo chmod 777 -R ~/tmp
 
+sudo ln /usr/bin/nodejs /usr/bin/node
+
+npm install
+
+bower install
+
+# Init dev sqlite database
+python ./src/modles.py
+
+# Init npm local package
+sudo npm install
+sudo pip install -r requeiments.pip
 # Run server
 grunt serve
+
